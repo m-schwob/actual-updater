fc4ad5e4f49afe8b391826f4bfd1805678aed56c
Menashe Schwob <menashe54@gmail.com>
2025-06-02 20:07:46 +0000

    GUI: - connect the gui with the OIDC. - add middleware and functionality to verify OIDC session. - check the authelia session cookie for existence and change. validating it against the authelia server still fails - verify the user token with userinfo endpoint.

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from nicegui import ui
 from src.app.oidc import OIDCAuth
+from starlette.middleware.sessions import SessionMiddleware
 
 from src.app.ui import AccountsManagerUI  # your UI setup function
 
@@ -10,7 +11,6 @@ app = FastAPI()
 # Initialize OIDC authentication
 oidc = OIDCAuth(
     app=app,
-    secret_key="fast-api-session-secret-key",
     client_id="actual-updater-client-id",
     client_secret="actual-updater-client-secret",
     server_url="http://127.0.0.1:9091"
@@ -19,7 +19,15 @@ oidc = OIDCAuth(
 # Bind FastAPI to NiceGUI
 ui.run_with(app)
 
+# Add session middleware globally for all session needs (must added last to be the first checked middleware in the stack)
+app.add_middleware(SessionMiddleware, secret_key="fast-api-session-secret-key")
+
 # Instantiate and start the UI
 password_manager_ui = AccountsManagerUI(account_list=["Bank A", "Bank B", "Bank C"])
 ui.page('/')(password_manager_ui.start_ui)
+
+# @ui.page('/')
+# def homepage():
+#     account_list=["Bank A", "Bank B", "Bank C"]
+#     return AccountsManagerUI(account_list).start_ui()
 
--- src/app/oidc.py
+++ src/app/oidc.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
-from starlette.middleware.sessions import SessionMiddleware
 from authlib.integrations.starlette_client import OAuth, StarletteOAuth2App
+from authlib.integrations.base_client.errors import InvalidTokenError
+import httpx
+
 
 class OIDCAuth:
-    def __init__(self, app: FastAPI, secret_key: str, client_id: str, client_secret: str, server_url: str):
+    def __init__(self, app: FastAPI, client_id: str, client_secret: str, server_url: str):
         self.app = app
-        self.app.add_middleware(SessionMiddleware, secret_key=secret_key)
         self.server_url = server_url
 
         self.oauth = OAuth()
@@ -20,30 +21,61 @@ class OIDCAuth:
             }
         )
 
-        self.setup_routes()
+        self._setup_routes()
+        self._register_oidc_middleware()
 
-    def setup_routes(self):
+    def _setup_routes(self):
         @self.app.get('/login')
         async def login(request: Request):
             redirect_uri = request.url_for('oidc_callback')
-            return await self.oauth.authelia.authorize_redirect(request, redirect_uri)
+            provider: StarletteOAuth2App = self.oauth.authelia
+            return await provider.authorize_redirect(request, redirect_uri)
 
         @self.app.get('/oidc/callback', name='oidc_callback')
         async def oidc_callback(request: Request):
             provider: StarletteOAuth2App = self.oauth.authelia
             token = await provider.authorize_access_token(request)
-            userinfo = await self.oauth.authelia.userinfo(token=token)
+            request.session['token'] = token
+            userinfo = await provider.userinfo(token=token)
             request.session['user'] = dict(userinfo)
+            request.session['authelia_session'] = request.cookies['authelia_session']
             return RedirectResponse(url='/')
-       
-        @self.app.get('/')
-        async def homepage(request: Request):
-            user = request.session.get('user')
-            if user:
-                return f"Hello, {user.get('name', 'User')}!"
-            return 'You are not logged in.'
-        
+           
         @self.app.get('/logout')
         async def logout(request: Request):
             request.session.clear()
             return RedirectResponse(url='/')
+                     
+    def _register_oidc_middleware(self):
+        @self.app.middleware('http')
+        async def oidc_middleware(request: Request, call_next):
+            if request.url.path in ['/login', '/oidc/callback', '/logout']:
+                return await call_next(request)
+            if await self._verify_authelia_session(request):
+                provider: StarletteOAuth2App = self.oauth.authelia
+                token = request.session.get('token')
+                try:
+                    userinfo = await provider.userinfo(token=token) # validate the user token
+                except InvalidTokenError:
+                    pass
+                if userinfo:
+                    return await call_next(request)
+            request.session.clear()
+            return RedirectResponse(url='/login') 
+     
+    async def _verify_authelia_session(self, request: Request):
+        authelia_session = request.cookies.get('authelia_session')
+        if not authelia_session or authelia_session != request.session.get('authelia_session'):
+            return False
+        
+        # async with httpx.AsyncClient() as client:
+        #     response = await client.get(
+        #         f"{self.server_url.rstrip('/')}/api/verify", headers={
+        #             'Cookie': f'authelia_session={authelia_session}'
+        #         }
+        #     )
+        #     if response.status_code != 200:
+        #         return False
+        return True
+            
+                

a7342b0f656ba45173f44a4dad7310c4ba552c74
rotembaris <rotemrotem@gmail.com>
2025-08-16 14:58:41 +0300

    fix config path for integration testing due to change in config testing

--- tests/service/index.test.ts
+++ tests/service/index.test.ts
@@ -4,7 +4,7 @@ import { main } from '../../src/service/index';
 describe('Service Index Integration Tests', () => {
     test('should run main function without error', async () => {
         console.log('Running main function...');
-
+        process.env.CONFIG_PATH = 'tests/sources/utils/validConfig.yaml';
         // Run the main function - it should complete without throwing
         await expect(main()).resolves.toBeUndefined();
 
